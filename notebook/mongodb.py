af317f88076561e83d4760b5eaef1306c7243522
AditiJanwade <136488800+AditiJanwade@users.noreply.github.com>
2025-02-26 21:07:54 +0530

    Updated

---
+++ notebook/mongodb.py
@@ -0,0 +1,9 @@
+'''
+mongodb+srv://aditti7977:0fMkFzUC0JxKRb2A@cluster0.ymh1j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0 
+
+'''
+
+import pandas as pd
+
+df=pd.read_csv("notebook/Visadataset.csv")
+print(df.head())
